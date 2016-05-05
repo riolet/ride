e6938c7a05231272a939d6d2d44a742eda9a179e
Tyler <tmanthegamer@gmail.com>
2016-05-04 19:18:15 -0700

    Added in QScintilla dependencies.

---
+++ ride-gui/mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    /*QQuickView *view = new QQuickView();
+    QWidget *container = QWidget::createWindowContainer(view, this);
+    container->setMinimumSize(200, 200);
+    container->setMaximumSize(200, 200);
+    container->setFocusPolicy(Qt::TabFocus);
+    view->setSource(QUrl("qrc:main.qml"));
+    ui->verticalLayout->addWidget(container);*/
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
