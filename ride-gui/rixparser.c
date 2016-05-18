54d0914472c0f8bb435009f0fd34d6a00541c243
Duy Pham <duy.phamthe91@gmail.com>
2016-05-17 21:24:46 -0700

    Fixed parser compilation error

--- ride-gui/rixparser.c
+++ ride-gui/rixparser.c
@@ -1,2 +1,13 @@
 #include "rixparser.h"
+#include "ride_parser/rixc.h"
 
+int Detect_errors(Error **err, int *err_num, char *doc)
+{
+	
+
+}
+
+int Return_errors()
+{
+
+}
--- ride-gui/rixparser.h
+++ ride-gui/rixparser.h
@@ -12,7 +12,22 @@ typedef struct error_object
 
 extern Error** errors;
 
+/**
+ * @brief      { function_description }
+ *
+ * @param      err      The err
+ * @param      err_num  The err num
+ * @param      doc      The doc
+ *
+ * @return     { description_of_the_return_value }
+ */
 extern int Detect_errors(Error** err, int* err_num, char* doc);
+
+/**
+ * @brief      { function_description }
+ *
+ * @return     { description_of_the_return_value }
+ */
 extern int Return_errors();
 
 #endif // ERROR_OBJECT_H
