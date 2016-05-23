7ff0a7761d4b92f0f31c48c4920c0002985f2820
Duy Pham <duy.phamthe91@gmail.com>
2016-05-22 17:55:31 -0700

    Checkpoint - Fixing error detection

--- ride-gui/ride_parser/parser.c
+++ ride-gui/ride_parser/parser.c
@@ -23,61 +23,67 @@ void sig_chld (int signo)
     return;
 }
 
-int main(int argc, char **argv)
-{
-    char        word_doc_buffer[10023];
-    Error     **error_array;
-    int        *error_number;
-    int doc, err;
+// int main(int argc, char **argv)
+// {
+   
+//     Error     **error_array;
+//     int        *error_number;
 
-    printf("Running parser!\n");
+//     printf("Running parser!\n");
 
-    /**
-    * Start to map semaphore struc with shared memory
-    */
-    while (true)
-    {
+//     printf("Map semaphore\n");
+//     sem_doc.sem         = sem_open(SEM_CODE,  0);
+//     sem_error.sem       = sem_open(SEM_ERROR, 0);
 
-        sem_doc.sem         = sem_open(SEM_CODE,  0);
-        sem_doc.fd          = shm_open(SHARED_CODE, O_RDONLY, 0666);
-        sem_doc.content     = (char *) mmap(0, 10240, PROT_READ, MAP_SHARED, sem_doc.fd,   0);
+//     if (sem_doc.sem == SEM_FAILED || sem_error.sem == SEM_FAILED)
+//     {
+//         perror("Semphore creation");
+//         return 1;
+//     }
 
-        sem_error.sem       = sem_open(SEM_ERROR, 0);
-        sem_error.fd        = shm_open(SHARED_ERROR, O_RDWR, 0666);
-        sem_error.content   = (Error **) mmap(0, 10240, PROT_READ, MAP_SHARED, sem_error.fd, 0);
+//     sem_post(sem_error.sem);
 
+//     /* ------------------------------------------------------------------------------------ */
 
-        // Checking for error
-        if (sem_doc.sem == SEM_FAILED
-                || sem_error.sem == SEM_FAILED
-                || sem_doc.fd == -1
-                || sem_error.fd == -1
-                || sem_error.errNumber == -1)
-        {
-            fprintf(stderr, "Parser could not start, error with shared memory.\n");
-            // return 1;
-        }
-        else
-        {
-            break;
-        }
+//     sem_wait(sem_doc.sem);
+//     printf("Map file descriptor\n");
+//     sem_doc.fd          = shm_open(SHARED_CODE, O_RDWR, 0666);
+//     sem_error.fd        = shm_open(SHARED_ERROR, O_RDWR, 0666);
 
-    }
+//     // Checking for error
+//     if (sem_doc.fd == -1 || sem_error.fd == -1)
+//     {
+//         fprintf(stderr, "Parser could not start, error with shared memory.\n");
+//         return 1;
+//     }
 
+//     sem_post(sem_error.sem);
 
-    /**
-     * Parser start to parse the code continuously
-     */
-    while (true)
-    {
-        printf("Waiting on document.\n");
-        sem_wait(sem_doc.sem);
-        printf("Woken up.\n");
-        // Doing stuff
-        errorDetect(sem_error.content, &(sem_error.errNumber), sem_doc.content);
-        printf("Error detection finished.\n");
-        sem_post(sem_error.sem);
-    }
+//     /* ------------------------------------------------------------------------------------ */
 
-    return 1;
-}
+//     sem_wait(sem_doc.sem);
+//     printf("Map contents with file descriptor\n");
+//     sem_doc.content     = (char *) mmap(0, 10240, PROT_READ, MAP_SHARED, sem_doc.fd,   0);
+//     sem_error.content   = (Error **) mmap(0, 10240, PROT_WRITE, MAP_SHARED, sem_error.fd, 0);
+
+//     sem_post(sem_error.sem);
+
+//     /**
+//      * Parser start to parse the code continuously
+//      */
+//     while (true)
+//     {
+//         printf("Waiting on document.\n");
+//         sem_wait(sem_doc.sem);
+//         printf("Woken up\n");
+
+//         printf("Print document:\n%s\n", sem_doc.content);
+
+//         errorDetect(error_array, &sem_error.errNumber, sem_doc.content);
+
+//         printf("Error detection finished\n");
+//         sem_post(sem_error.sem);
+//     }
+
+//     return 1;
+// }
